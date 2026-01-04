1d4ece417702747bcaa4d74286f0043ad47dfc42
a1exfox <alex.fox.personal@gmail.com>
2026-01-04 19:28:11 +0300

    030 set layout, template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,13 +7,12 @@ import (
 	"github.com/a1exfox/go-course/pkg/handlers"
 )
 
-const portNumber = ":8080"
+const portNumber = "localhost:8080"
 
 func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
-	fmt.Printf("Starting application on port %s", portNumber)
-
+	fmt.Printf("Starting application on port %s\n", portNumber)
 	http.ListenAndServe(portNumber, nil)
 }
