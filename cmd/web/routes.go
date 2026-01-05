f7c1eda8721563e79297561d03d789606b072e4c
a1exfox <alex.fox.personal@gmail.com>
2026-01-05 17:02:11 +0300

    036 using pat routing

---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/a1exfox/go-course/pkg/config"
+	"github.com/a1exfox/go-course/pkg/handlers"
+	"github.com/bmizerany/pat"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+    mux := pat.New()
+    mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+    mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+    return mux
+}
